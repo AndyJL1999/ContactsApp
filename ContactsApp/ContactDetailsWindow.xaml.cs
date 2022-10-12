6e4384326b2342170be9bb152351d43cd65b6173
AndyJL1999 <andylainez115@hotmail.com>
2022-10-12 15:13:55 -0400

    Update ContactDetailsWindow.xaml.cs

--- ContactsApp/ContactDetailsWindow.xaml.cs
+++ ContactsApp/ContactDetailsWindow.xaml.cs
@@ -29,23 +29,30 @@ namespace ContactsApp
         public ContactDetailsWindow(Contact contact)
         {
             InitializeComponent();
+            
+            //Set the information for all of the text boxes and setting the contact to the selected contact
             _contact = contact;
             nameTextBox.Text = _contact.Name;
             emailTextBox.Text = _contact.Email;
             phoneNumberTextBox.Text = _contact.Phone;
             imgUrlTextBlock.Text = _contact.ImageUrl;
 
+            //Set the owner for the contact details window
             Owner = Application.Current.MainWindow;
+            
+            //Set the location for the contact details window to the center of it's owner
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
         }
 
         private async void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
+            //Set the new information into the Contact object
             _contact.Name = nameTextBox.Text;
             _contact.Email = emailTextBox.Text;
             _contact.Phone = phoneNumberTextBox.Text;
             _contact.ImageUrl = imgUrlTextBlock.Text;
 
+            //Make a Api call to update the contact using ApiHelper
             using HttpResponseMessage response = await ApiHelper.ApiClient.PutAsJsonAsync(ApiHelper.ApiClient.BaseAddress + "api/Contacts/Update", _contact);
 
             Close();
@@ -69,10 +76,14 @@ namespace ContactsApp
 
         private void ChangeImageButton_Click(object sender, RoutedEventArgs e)
         {
+            //Opens file explorer
             OpenFileDialog dialog = new OpenFileDialog();
+            
+            //Setting filter for the files that will be shown
             dialog.Filter = "Image files (*.png; *.jpg)|*.png;*.jpg;*.jpeg";
             dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
 
+            //If file explorer opens successfully set the chosen file as the imgUrl text
             if (dialog.ShowDialog() == true)
             {
                 string fileName = dialog.FileName;
