5b14072cfd2416d30ba4f7d8d9e03a8db5e0ef28
AndyJL1999 <andylainez115@hotmail.com>
2022-10-12 15:24:20 -0400

    Update ApiHelper.cs

--- ContactsAPI/Helpers/ApiHelper.cs
+++ ContactsAPI/Helpers/ApiHelper.cs
@@ -7,6 +7,10 @@ namespace ContactsAPI.Helpers
     {
         public static HttpClient ApiClient { get; set; }
 
+        /// <summary>
+        /// Creates an HttpClient to allow calls to be made to the API and initializes a global variable called 'ApiClient'.
+        /// Use 'ApiClient' to make API calls. Should only be used once: in the constructor that is at the start of the application.
+        /// </summary>
         public static void InitializeClient()
         {
             ApiClient = new HttpClient();
